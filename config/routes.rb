9b9878427a81ffcca5c3d04c34de62e2b6de932d
Yahor Zhylinski <yahor.zhylinski@gmail.com>
2016-04-20 02:00:37 +0300

    add 2 main routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    get 'sensor_values' => "sensor_values#index"
+    post 'sensor_values/upload_data' => "sensor_values#upload_data"
+  end
+
 end
